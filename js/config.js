7eed89691e3db223e85f2809c937c9f290875f91
Marti Serra <martiserramolina99@gmail.com>
2022-04-22 02:26:24 +0200

    Change styles

--- js/config.js
+++ js/config.js
@@ -5,25 +5,25 @@ export const config = {
       font: {
         family: "Raleway, sans-serif",
         size: 30,
-        color: "#70716e",
+        color: "#626262",
       },
       background: "rgba(0,0,0,0)",
-      lines: { color: "#cacbc5", outside: false },
+      lines: { color: "#E0E0E0", outside: false },
     },
     button: {
-      color: "#70716e",
-      background: "#f6f6f4",
-      border: { size: 1, color: "#cacbc5" },
-      corner: { type: "round", size: 5 },
+      color: "#626262",
+      background: "#F5F5F5",
+      border: { size: 5, color: "#E0E0E0" },
+      corner: { type: "round", size: 10 },
       font: {
         family: "Raleway, sans-serif",
         size: 16,
         weight: 600,
       },
-      mousedown: { background: "#dddddc" },
+      mousedown: { background: "##E0E0E0" },
     },
     text: {
-      color: "#70716e",
+      color: "#626262",
       font: {
         family: "Raleway, sans-serif",
         size: 16,
